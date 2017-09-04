f49488e9c63f129e2d33c0cbf4d8e96597e375ca
Andrey Larin <larin.projects@gmail.com>
2017-09-04 17:07:14 +0300

    Added answer mark view tests

--- qa/tests.py
+++ qa/tests.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.test import TestCase, RequestFactory
 
 from qa.models import Question, Answer, User
-from qa.views import QuestionVoteView, AnswerVoteView
+from qa.views import QuestionVoteView, AnswerVoteView, AnswerMarkView
 
 
 def create_question(author, title='Test question', text='Test question text'):
@@ -15,7 +15,73 @@ def create_answer(question, author, text='Test answer text'):
     return Answer.objects.create(text=text, question=question, author=author)
 
 
-class VoteViewTests(TestCase):
+class AnswerMarkViewTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+        self.question_author = User.objects.create_user(
+            username='test1', email='test1@eamil.com', password='top_secret'
+        )
+        self.answer_author = User.objects.create_user(
+            username='test2', email='test2@eamil.com', password='top_secret'
+        )
+        self.question = create_question(author=self.question_author)
+        self.answer = create_answer(author=self.answer_author, question=self.question)
+
+    def test_unauthorized_user_can_not_mark_answer(self):
+        response = self.client.post(self.answer.get_mark_url())
+        self.assertEqual(response.status_code, 403)
+
+    def test_not_question_author_can_not_mark_answer(self):
+        request = self.factory.post(self.answer.get_mark_url())
+
+        request.user = self.answer_author
+
+        self.assertEqual(self.question.has_answer, False)
+        self.assertEqual(self.answer.is_correct, False)
+
+        response = AnswerMarkView.as_view()(request, self.answer.pk)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(Question.objects.get(pk=self.question.pk).has_answer, False)
+        self.assertEqual(Answer.objects.get(pk=self.answer.pk).is_correct, False)
+
+    def test_question_author_can_mark_answer(self):
+        request = self.factory.post(self.answer.get_mark_url())
+
+        request.user = self.question_author
+
+        self.assertEqual(self.question.has_answer, False)
+        self.assertEqual(self.answer.is_correct, False)
+
+        response = AnswerMarkView.as_view()(request, self.answer.pk)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(Question.objects.get(pk=self.question.pk).has_answer, True)
+        self.assertEqual(Answer.objects.get(pk=self.answer.pk).is_correct, True)
+
+    def test_question_author_can_unmark_answer(self):
+        request = self.factory.post(self.answer.get_mark_url())
+
+        request.user = self.question_author
+
+        self.assertEqual(self.question.has_answer, False)
+        self.assertEqual(self.answer.is_correct, False)
+
+        response = AnswerMarkView.as_view()(request, self.answer.pk)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(Question.objects.get(pk=self.question.pk).has_answer, True)
+        self.assertEqual(Answer.objects.get(pk=self.answer.pk).is_correct, True)
+
+        response = AnswerMarkView.as_view()(request, self.answer.pk)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(Question.objects.get(pk=self.question.pk).has_answer, False)
+        self.assertEqual(Answer.objects.get(pk=self.answer.pk).is_correct, False)
+
+
+class VoteViewsTests(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
 
